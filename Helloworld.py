ba8b604b1468a5e30638e1b9089be7b7ffa880b4
JC-EVA02 <114107926+JC-EVA02@users.noreply.github.com>
2022-10-01 17:18:17 -0400

    Update Helloworld.py

--- Helloworld.py
+++ Helloworld.py
@@ -3,6 +3,9 @@ from datetime import date
 today = date.today()
 todaysDate = today.strftime("%m/%d/%y")
 username =  input("What is your name?")
+  
+
+  
 
 print("Hello " + username + "!" + " Today's Date is: ", todaysDate)
 
